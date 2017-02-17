b61a843e644c7873cf52df71e9392c6286387b51
Daryll <daryllcsh@gmail.com>
2017-02-17 13:31:13 -0800

    Bold text on hover

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
-var App = () => (
+let App = () => (
   <div>
     <h2>Grocery List</h2>
-    <GroceryList/>
+    <GroceryList list={['Banana', 'Milk', 'Chicken']}/>
   </div>
 );
 
-let GroceryList = () => (
+let GroceryList = (props) => (
   <ul>
-    <Banana/>
-    <Milk/>
+    {props.list.map(item => 
+      <GroceryListItem item={item}/>
+    )}
   </ul>
 );
 
-let Banana = () => (
-  <li>Banana</li>
-);
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false,
+      hovered: false
+    };
+  }
 
-let Milk = () => (
-  <li>Milk</li>
-);
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  onMouseOver() {
+    this.setState({
+      hovered: true
+    });
+  }
+
+  onMouseOut() {
+    this.setState({ 
+      hovered: false
+   });
+ }
+
+  render() {
+    let style = {
+      textDecoration: this.state.done? 'line-through' : 'none',
+      fontWeight: this.state.hovered? 'bold' : 'none'
+    };
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)} onMouseOver={this.onMouseOver.bind(this)} onMouseOut={this.onMouseOut.bind(this)}>{this.props.item}</li>
+    );
+  }
+
+}
 
 ReactDOM.render(<App/>, document.getElementById('app'));
