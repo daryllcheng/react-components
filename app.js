500aea7c0beeddd031d7afb817fc274659ff8276
Daryll <daryllcsh@gmail.com>
2017-02-17 12:25:23 -0800

    Render nested components

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList/>
+  </div>
+);
+
+let GroceryList = () => (
+  <ul>
+    <Banana/>
+    <Milk/>
+  </ul>
+);
+
+let Banana = () => (
+  <li>Banana</li>
+);
+
+let Milk = () => (
+  <li>Milk</li>
+);
+
+ReactDOM.render(<App/>, document.getElementById('app'));
